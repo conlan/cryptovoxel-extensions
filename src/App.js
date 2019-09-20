28d99fa876d9fa8027ada96338b55690621137d9
conlan <conlan@gmail.com>
2019-09-19 23:35:59 -0700

    Add some text for displaying on the home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React, { useEffect } from 'react'
 import { useWeb3Context } from 'web3-react'
 
-// Functions
-import { executeColorVendingV1 } from './functions/color_vending/1/index'
-
-// map an intention type + version to a function
-const functionMap = {
-	"colr-vending/1" : executeColorVendingV1
-}
-
-// only initiate the transaction once
-var didExecuteIntent = false;
+import ColorVendingV1 from './functions/color_vending/1/index'
 
 export default function App (props) {
   // get the web3 context
@@ -26,21 +17,20 @@ export default function App (props) {
   } else if (context.error) {
     // error
     return <p>{context.error}</p>
-  } else {
-    // success. check that we didn't already intitiate the transaction
-    if (didExecuteIntent === false) {
+  } else {  
 		var intentType = props.intentData.intent;
 		var intentVersion = props.intentData.version;
+    var path = intentType + intentVersion
 
-		var functionData = {
-			"context" : context,
-			"intentData" : props.intentData
-		}
-
-		functionMap[intentType + "/" + intentVersion](functionData)
-
-		didExecuteIntent = true;
+    var componentData = {
+      "intentData" : props.intentData,
+      "context" : context
     }
-    return <p>success</p>
+
+    if (path === "colr-vending1") {
+      return <ColorVendingV1 props={componentData}/>
+    } else {
+      return <div/>
+    }
   }
 }
--- src/functions/color_vending/1/index.js
+++ src/functions/color_vending/1/index.js
@@ -1,30 +1,78 @@
+import React, { Component } from 'react'
+
 const ethers = require('ethers');
 
-var uniswap_contract_abi = [{"name":"ethToTokenSwapOutput","inputs":[{"type":"uint256","name":"tokens_bought"},{"type":"uint256","name":"deadline"}],"type":"function","outputs":[{"type":"uint256","name":"out"}],"constant":false,"payable":true,"gas":50463}]
+var uniswap_contract_abi = [{"name":"ethToTokenSwapInput","inputs":[{"type":"uint256","name":"min_tokens"},{"type":"uint256","name":"deadline"}],"type":"function","outputs":[{"type":"uint256","name":"out"}],"constant":false,"payable":true,"gas":12757},{"name":"getEthToTokenOutputPrice","inputs":[{"type":"uint256","name":"tokens_bought"}],"type":"function","outputs":[{"type":"uint256","name":"out"}],"constant":true,"payable":false,"gas":6872}]
 var uniswap_contract_address = '0x3F0c63dA66457dedc2677BEF6bbdd457BA7A3C0b'
 
-export function executeColorVendingV1(functionData) {
-	const signer = functionData.context.library.getSigner();
+// only initiate the transaction once
+var didExecuteIntent = false;
+
+var web3context = null;
+
+var intendedColrAmount = 0
+
+async function get_exchange_rate_and_swap() {
+	var exchangeContract = new ethers.Contract(uniswap_contract_address, uniswap_contract_abi, web3context.library)
+
+	// get the exchange rate for the desired COLR amount
+	var exchangeRateWei = await exchangeContract.getEthToTokenOutputPrice(intendedColrAmount)
+
+	// convert to ETH
+	var exchangeRateEth = exchangeRateWei / 10 ** 18
 	
-	var intentValue = functionData.intentData.value;	
-
-	// take current time in seconds + 5 minutes
-	var deadline = Math.round(new Date().getTime() / 1000) + 300;
-
 	// get contract interface
 	var iface = new ethers.Interface(uniswap_contract_abi)	
 
-	// generate the call data for how much COLR we want to buy
-	var calldata = iface.functions.ethToTokenSwapOutput.encode([
-		intentValue, deadline
-	])
+	// take current time in seconds + 5 minutes
+	var deadline = Math.round(new Date().getTime() / 1000) + 300;	
 
-	// expected amount to pay
-	var expectedEthInput = "0.005" // TODO convert this
+	console.log(typeof intendedColrAmount)
+	// generate the call data for the exchange rate -- amount of eth we'll need
+	var calldata = iface.functions.ethToTokenSwapInput.encode([100, deadline])
 
- // 	signer.sendTransaction({
+	console.log(calldata)
+
+	// get the signer
+	var signer = web3context.library.getSigner();
+
+	// send the transaction
+	// signer.sendTransaction({
 	// 	to: uniswap_contract_address,
 	// 	data: calldata,
-	// 	value: ethers.utils.parseEther(expectedEthInput)
+	// 	value: ethers.utils.parseEther(exchangeRateEth.toString())
 	// })
 }
+
+class ColorVendingV1 extends Component {
+	constructor(props) {
+		super(props)
+
+		// set the web3 context
+		web3context = this.props.props.context	
+
+		// set the intended colr purchase amount
+		intendedColrAmount = this.props.props.intentData.value;
+	}
+
+	componentDidMount() {
+		if (didExecuteIntent === false) {			
+			didExecuteIntent = true;
+
+			get_exchange_rate_and_swap()		
+		} 
+	}
+
+	render() {
+		var etherscanLink = "https://etherscan.io/address/" + uniswap_contract_address
+    	return (<div>
+    		<p>Thank you for using the $COLR vending machine! Your have initiated a transaction for {intendedColrAmount} $COLR.</p>    		
+    		<p>You can verify the $COLR uniswap exchange at this address: <b><a target="_blank" href={etherscanLink}>https://etherscan.io/address/{uniswap_contract_address}</a></b></p>
+    		<p>Please be sure to check all inputs and gas value before submitting your transaction.</p>
+    		<p>Close this window to return to Cryptovoxels.</p>
+    		</div>
+		)
+	}
+}
+
+export default ColorVendingV1
